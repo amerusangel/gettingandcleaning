e4c0992ceff6cac17507f12bd9ef48e9835e92fd
amerusangel <amerusangel@yahoo.fr>
2016-08-14 17:41:35 +0100

    Add CodeBook.md

---
+++ run_analysis.R
@@ -0,0 +1,47 @@
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl,destfile="./data/Dataset.zip")
+unzip(zipfile="./data/Dataset.zip",exdir="./data")
+path_rf <- file.path("./data" , "UCI HAR Dataset")
+dt0<-list.files(path_rf, recursive=TRUE)
+dt0
+ActivityTest  <- read.table(file.path(path_rf, "test" , "Y_test.txt" ),header = FALSE)
+ActivityTrain <- read.table(file.path(path_rf, "train", "Y_train.txt"),header = FALSE)
+SubjectTrain <- read.table(file.path(path_rf, "train", "subject_train.txt"),header = FALSE)
+SubjectTest  <- read.table(file.path(path_rf, "test" , "subject_test.txt"),header = FALSE)
+str(ActivityTest)
+str(ActivityTrain)
+str(SubjectTrain)
+str(SubjectTest)
+FeaturesTest  <- read.table(file.path(path_rf, "test" , "X_test.txt" ),header = FALSE)
+FeaturesTrain <- read.table(file.path(path_rf, "train", "X_train.txt"),header = FALSE)
+str(FeaturesTest)
+str(FeaturesTrain)
+Subject <- rbind(SubjectTrain, SubjectTest)
+Activity<- rbind(ActivityTrain, ActivityTest)
+Features<- rbind(FeaturesTrain, FeaturesTest)
+names(Subject)<-c("subject")
+names(Activity)<- c("activity")
+FeaturesNames <- read.table(file.path(path_rf, "features.txt"),head=FALSE)
+names(Features)<- FeaturesNames$V2
+Combine <- cbind(Subject, Activity)
+Data <- cbind(Features, Combine)
+subdataFeaturesNames<-FeaturesNames$V2[grep("mean\\(\\)|std\\(\\)", FeaturesNames$V2)]
+selectedNames<-c(as.character(subdataFeaturesNames), "subject", "activity" )
+Data<-subset(Data,select=selectedNames)
+str(Data)
+activityLabels <- read.table(file.path(path_rf, "activity_labels.txt"),header = FALSE)
+head(Data$activity,30)
+names(Data)<-gsub("^t", "time", names(Data))
+names(Data)<-gsub("^f", "frequency", names(Data))
+names(Data)<-gsub("Acc", "Accelerometer", names(Data))
+names(Data)<-gsub("Gyro", "Gyroscope", names(Data))
+names(Data)<-gsub("Mag", "Magnitude", names(Data))
+names(Data)<-gsub("BodyBody", "Body", names(Data))
+names(Data)
+library(plyr);
+Data2<-aggregate(. ~subject + activity, Data, mean)
+Data2<-Data2[order(Data2$subject,Data2$activity),]
+write.table(Data2, file = "tidydata.txt",row.name=FALSE)
+library(knitr)
+knit2html("codebook.Rmd");
